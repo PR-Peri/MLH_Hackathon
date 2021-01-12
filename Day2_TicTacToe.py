a320c774c174c30bf7cf2f6c80bac06c85566289
PR <46223444+Perivitta@users.noreply.github.com>
2021-01-12 12:54:59 +0800

    Update Day2_TicTacToe.py

--- Day2_TicTacToe.py
+++ Day2_TicTacToe.py
@@ -1,4 +1,9 @@
-# Tic-Tac-Toe game in Python.
+#Implementation of Two Player Tic-Tac-Toe game in Python.
+
+''' We will make the board using dictionary 
+    in which keys will be the location(i.e : top-left,mid-right,etc.)
+    and initialliy it's values will be empty space and then after every move 
+    we will change the value according to player's choice of move. '''
 
 theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
             '4': ' ' , '5': ' ' , '6': ' ' ,
@@ -9,6 +14,10 @@ board_keys = []
 for key in theBoard:
     board_keys.append(key)
 
+''' We will have to print the updated board after every move in the game and 
+    thus we will make a function in which we'll define the printBoard function
+    so that we can easily print the board everytime by calling this function. '''
+
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
     print('-+-+-')
@@ -16,12 +25,17 @@ def printBoard(board):
     print('-+-+-')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
 
+# Now we'll write the main function which has all the gameplay functionality.
 def game():
+
     turn = 'X'
     count = 0
+
+
     for i in range(10):
         printBoard(theBoard)
         print("It's your turn," + turn + ".Move to which place?")
+
         move = input()        
 
         if theBoard[move] == ' ':
@@ -31,6 +45,7 @@ def game():
             print("That place is already filled.\nMove to which place?")
             continue
 
+        # Now we will check if player X or O has won,for every move after 5 moves. 
         if count >= 5:
             if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
                 printBoard(theBoard)
@@ -84,10 +99,12 @@ def game():
         else:
             turn = 'X'        
     
-    restart = input("Do wish to play Again?(y/n)")
+    # Now we will ask if player wants to restart the game or not.
+    restart = input("Do want to play Again?(y/n)")
     if restart == "y" or restart == "Y":  
         for key in board_keys:
             theBoard[key] = " "
+
         game()
 
 if __name__ == "__main__":
